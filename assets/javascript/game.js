0e48af5706a62546c13119f9446135a83beb3c8b
Caroline Lester <carolineclester@gmail.com>
2020-04-25 20:05:24 -0400

    code is complete except for some simple debugging

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,110 @@ $(document).ready(function() {
     var score = 0;
 
 // random number generator for the Match: (19-120)
-var ComputerGuess = parseInt(Math.floor(Math.random() * 120) + 19);
-$("randomNumber").html(ComputerGuess);
+var computerGuess = parseInt(Math.floor(Math.random() * 102) + 19);
+$("#randomNumber").html(computerGuess);
 
-var strawberry = parseInt(Math.floor(Math.random() * 12) +2);
-var blueberry = parseInt(Math.floor(Math.random() * 12) +2);
-var carrot = parseInt(Math.floor(Math.random() * 12) +2);
-var cucumber = parseInt(Math.floor(Math.random() * 12) +2);
+// change to integer
+var strawberry = parseInt(Math.floor(Math.random() * 11) +2);
+var blueberry = parseInt(Math.floor(Math.random() * 11) +2);
+var carrot = parseInt(Math.floor(Math.random() * 11) +2);
+var cucumber = parseInt(Math.floor(Math.random() * 11) +2);
 
+// test before continuing
+console.log("Match: " + computerGuess);
+console.log("strawberries: " + strawberry);
+console.log("blueberries: " + blueberry);
+console.log("carrots: " + carrot);
+console.log("cucumbers: " + cucumber);
 
-}):
+function reset () { //trigger to reset game 
+
+    score = 0;
+    $("#score").html(score);
+    var computerGuess = parseInt(Math.floor(Math.random() * 102) + 19);
+    $("#randomNumber").replaceWith(computerGuess);
+    console.log("Match: " + computerGuess);
+    
+
+    strawberry = parseInt(Math.floor(Math.random() * 11) +2);
+    blueberry = parseInt(Math.floor(Math.random() * 11) +2);
+    carrot = parseInt(Math.floor(Math.random() * 11) +2);
+    cucumber = parseInt(Math.floor(Math.random() * 11) +2);
+
+    console.log("Match: " + computerGuess);
+    console.log("strawberries: " + strawberry);
+    console.log("blueberries: " + blueberry);
+    console.log("carrots: " + carrot);
+    console.log("cucumbers: " + cucumber);
+};
+
+$("#strawberries").on("click", function() {
+    score = score + strawberry;
+    $("#score").html(score);
+    console.log("score1: " + score);
+
+    if (score == computerGuess) {
+        checkifWon();
+    }
+    else if (score > computerGuess) {
+        checkifWon();
+    }
+    
+});
+
+$("#blueberries").on("click", function() {
+    score = score + blueberry;
+    $("#score").html(score);
+    console.log("score2: " + score);
+
+    if (score == computerGuess) {
+        checkifWon();
+    }
+
+    else if (score >= computerGuess) {
+        checkifWon();
+    }
+});
+
+$("#carrots").on("click", function() {
+    score = score + carrot;
+    $("#score").html(score);
+    console.log("score3: " + score);
+    if (score == computerGuess) {
+        checkifWon();
+    }
+    else if (score >= computerGuess) {
+        checkifWon();
+    }
+});
+
+$("#cucumbers").on("click", function() {
+    score = score + cucumber;
+    $("#score").html(score);
+    console.log("score4: " + score);
+    if (score == computerGuess) {
+        checkifWon();
+    }
+    else if (score >= computerGuess) {
+        checkifWon();
+    }
+});
+
+function checkifWon() {
+    if (score == computerGuess) {
+        wins++
+        console.log(wins);
+        alert("You've Won! Congrats!");
+        $("#wins").replaceWith(wins);
+        reset ();
+    }
+    else if (score > computerGuess) {
+        losses++
+        console.log(losses);
+        alert("You've lost! Try Again!");
+        $("#losses").replaceWith(losses);
+        reset();
+    }
+}
+
+});
